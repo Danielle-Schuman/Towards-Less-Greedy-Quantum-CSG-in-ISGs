1ef4a98d07ea6a07b0c5a976ff2ee763cd8c18b2
Schuman, Daniëlle <d.schuman@campus.lmu.de>
2024-04-17 12:37:44 +0200

    Wrote READ_ME and made main pretty

---
+++ READ_ME.md
@@ -0,0 +1,59 @@
+# Code for the thesis "Towards Less Greedy Quantum Coalition Structure Generation in Induced Subgraph Games"
+
+This project contains the code for the master's thesis entitled 
+"Towards Less Greedy Quantum Coalition Structure Generation in Induced Subgraph Games"
+which was written by Daniëlle Schuman.
+
+Most parts of the code in this project have been written by Daniëlle Schuman as well, with a few exceptions:
+- Some methods in this project have been written by, or are adapted from, Code written by Jonas Nüßlein. These Methods are marked accordingly.
+- The code in the folder `classical/best-partition-master` has been downloaded from `https://github.com/Alexander-Belyi/best-partition`. It has been written by Alexander Belyi as part of his paper "[Subnetwork Constraints for Tighter Upper Bounds and Exact Solution of the Clique Partitioning Problem](https://arxiv.org/abs/2110.05627)" and we introduced only a very small number of modifications to it which are marked accordingly.
+- For some methods, especially a large part of those contained in the file `plotting.py`, we used the Large-Language Model ChatGPT (https://chat.openai.com/auth/login) to generate boilerplate code for basic functionalities like the reading and writing of files, the parsing of lists, dicts and strings or the generation of matplotlib plots. We subsequently checked this code carefully to ensure its correct functionality, and subsequently adapted it to enhance its functionality (fixing bugs where necessary), improve readability regarding semantics and at times improve efficiency.
+
+
+## Installing and running the code
+
+### Quantum(-inspired) code
+
+To run the quantum and quantum-inspired code in this project, first install the requirements using
+
+```
+pip install -r requirements.txt
+```
+Subsequently, install the minorminer package using
+```
+pip install minorminer==0.2.13
+```
+and ignore the resulting dependency conflict.
+
+If you want to run the version of the code using the D-Wave quantum annealer, you furthermore need to
+- configure the UQO middleware according to the instructions in `secrets_folder/configure_uqo.py`.
+- create a file called `dwave_token.py` in the `secrets_folder`. This file needs to contain one line with `TOKEN = '...'` where ... is the token of your D-Wave Leap account. You can create a free D-Wave Leap account at https://cloud.dwavesys.com/leap/signup/. (Our algorithm will not use any of your QPU runtime, it just needs cloud access to up-to-date information about the quantum annealer's hardware graph.)
+
+Our algorithms can then be run using:
+```
+python main.py
+```
+This can be passed the options:
+- `--seed`: pass an integer to use as a custom seed for the simulator. If no seed is passed, the code will run all seeds from our experiments of the respective solver.
+- `--solver`: The solver to run the algorithms with. Options are `"qbsolv"`, `"qaoa"`or `"dwave"`. The first one is the default.
+
+Notice that the algorithms have long runtimes on either solver.
+
+### Classical baseline
+To install run the Belyi's code, which we used as a baseline, on our data, perform the following steps (mostly taken from Belyi's READ_ME):
+
+1. Install [CMake](https://cmake.org/download/);
+2. Install [CBC](https://github.com/coin-or/Cbc) as a solver. To do that, follow the instructions from the [official page](https://github.com/coin-or/Cbc). On Mac, Belyi et al. recommend using [Homebrew](https://brew.sh/), on ubuntu/debian use ```sudo apt install coinor-libcbc-dev```. Subsequently, adapt `classical/best-partition-master/cmake/Modules/FindCBC.cmake`, by replacing the paths to the different necessary components of CBC with those they have on your computer.
+3. You can also (or additionally) use CPLEX if you have it (we did for our experiments). CMake should find it if installed in the default location, otherwise provide the path in `CPLEX_ROOT`.
+4. Now, delete all contents from `classical/best-partition-master/build`. Then navigate to this folder on your command line and run CMake: 
+```bash
+cmake ..
+```
+5. Run ```make``` on your commandline;
+6. Run built program: ```./BestPartition```.
+
+
+## Our experiments
+Notice that while the current configuration of `main.py` should be usable to reproduce our experiments, we used several older versions of the code with hardcoded configurations (especially regarding what graphs to run when) to produce the results in the thesis. A lot of them can be found in the other files in this project whose name starts with `main_`.
+
+The results from our experiments can be found in the folder `results` in this project, with the respective command line outputs in `run_txt`. Visualizations of our experiments can be found in `plots`.
--- main.py
+++ main.py
@@ -76,19 +76,13 @@ def main(algorithm_list, data, graph_sizes, num_graphs_per_size, experiment, dir
     run_id = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '-')
 
     for algorithm in algorithm_list:
-        #too_large = False
+        too_large = False  # only used in qaoa (or dwave)
         if isinstance(algorithm, IterativeQuantumAlgorithmWithK):
-            if(isinstance(algorithm, ours_iterative_exactly) and algorithm.k == 13):
-                agents = [14,16,18,20,22,24,26,28]
-            else:
-                agents = graph_sizes
+            agents = graph_sizes
             for num_agents in agents:
                 if algorithm.k <= num_agents:
                     print(f"\n\n\nTest for graphsize {num_agents}")
-                    if (isinstance(algorithm, ours_iterative_exactly) and algorithm.k == 13 and num_agents == 14):
-                        this_range = range(17, num_graphs_per_size)
-                    else:
-                        this_range = range(num_graphs_per_size)
+                    this_range = range(num_graphs_per_size)
                     for graph_num in this_range:
                         print(f"\n\n     Graph {graph_num}")
                         graph = data[num_agents][graph_num]
@@ -117,12 +111,13 @@ def main(algorithm_list, data, graph_sizes, num_graphs_per_size, experiment, dir
                             subprocess.run(command, check=True)
                         except subprocess.CalledProcessError as e:
                             print("                    Error: Running algorithm failed, most likely due to insufficient available memory. Error message: ", e)
-                            #too_large = True
-                            #break
-                #if too_large:
-                    #break
-                #else:
-                    #pass
+                            too_large = True
+                            if algorithm.solver == "qaoa":
+                                break
+                if too_large and algorithm.solver == "qaoa":
+                    break
+                else:
+                    pass
         else:
             for num_agents in graph_sizes:
                 print(f"\n\n\nTest for graphsize {num_agents}")
@@ -155,37 +150,37 @@ def main(algorithm_list, data, graph_sizes, num_graphs_per_size, experiment, dir
                         subprocess.run(command, check=True)
                     except subprocess.CalledProcessError as e:
                         print("                    Error: Running algorithm failed, most likely due to insufficient available memory. Error message: ", e)
-                        #too_large = True
-                        #break
-                #if too_large:
-                    #break
+                        too_large = True
+                        if (algorithm.solver == "qaoa" or algorithm.solver == "dwave"):
+                            break
+                if too_large and (algorithm.solver == "qaoa" or algorithm.solver == "dwave"):
+                    break
     print(f"Done running tests for {experiment}.")
-
-    '''
-    # TODO: Move to plotting.py
-    algorithm.values_sums[i] += value # -> TODO: Do for all values
-    print("Value sums:")
-    # TODO: come up with new, better plotting that puts them all in one plot instead of doing 1v1-comparison
-    for a1 in range(len(algorithm_list)):
-        print(algorithm_list[a1].name, ": ", [np.round(z, 3) for z in algorithm_list[a1].values_sums])
-        for a2 in range(a1 + 1, len(algorithm_list)):
-            # TODO: Create results_dict from algoritms.data before (or change plotting method -> probably better)
-            plotting.get_barchart_win_lose_draw(algo1_name=algorithm_list[a1].name, algo2_name=algorithm_list[a2].name, results_dict=results_dict, seed=seed, note="qbsolv")
-    print("Done plotting results.")
-    '''
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", type=int, required=True)
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--solver", type=str, default="qbsolv")  # alternatively "qaoa" or "dwave"
     args = parser.parse_args()
-    # TODO: Determine sensible number of seeds for statistical significance
-    num_seeds = 1
-    for _ in range(num_seeds):
+
+    solvers = [args.solver]
+
+    # TODO: Determine sensible number of seeds needed for statistical significance
+    if args.seed:
+        seeds = [args.seed]
+    elif solvers[0] == "qbsolv":
+        seeds = [3949468976, 2223921875, 1665964778, 1980386705, 1364436995, 2881126339, 2777013863, 3875681486, 4038098247,
+         3748732874]
+    elif solvers[0] == "qaoa":
+        seeds = [3949468976, 2223921875, 1665964778, 1980386705, 1364436995, 2881126339]
+    elif solvers[0] == "dwave":
+        seeds = [0]  # Seed not relevant for D-Wave
+    else:  # does not happen, but code below was used to create seeds in list of qbsolv
+        seeds = [random.randint(0, 2 ** 32 - 1)]
+
+    for seed in seeds:
         # Setting the seed
-        #seed = random.randint(0, 2 ** 32 - 1)
-        #seed = 0  # Seed not relevant for D-Wave
-        seed = args.seed
         random.seed(seed)
         np.random.seed(seed)
         print(f"Seed: {seed}")
@@ -203,33 +198,30 @@ if __name__ == "__main__":
         num_graphs_per_size = len(data[graph_sizes[0]])  # is 20 for E.ON data
         num_graph_sizes = len(graph_sizes)
 
-        # Simulate
-        solvers = ["qbsolv"] #"qaoa"]
-        parallel = [True]  #, False] -> Try sequential later (maybe)
-        k_list = [i for i in range(13, graph_sizes[-1])]
-
-        # D-Wave -> uncomment this and comment simulate for running with D-Wave
-        '''
-        solvers = ["dwave"]
-        parallel = [True]
-        k_list = [4]
-        '''
+        parallel = [True]  # , False] -> Try sequential later (maybe)
 
         for solver in solvers:
-            '''
-            algorithm_list = [Jonas(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
-                              Danielle(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
-                              n_split_GCSQ(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
-                              r_qubo(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver)
-                              ]
+            if solver == "qbsolv":
+                k_list = [i for i in range(2, graph_sizes[-1])]
+            else:
+                k_list = [4, 3, 5, 2]
+            if solver == "dwave":
+                algorithm_list = [Jonas(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
+                                  Danielle(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
+                                  r_qubo(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver)
+                                  ]
+            else:
+                algorithm_list = [Jonas(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
+                                  Danielle(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
+                                  n_split_GCSQ(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver),
+                                  r_qubo(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver)
+                                  ]
             directory = f"results/{data_name}/quantum/{solver}"
             directory_exists = create_nested_directory(directory)
             if directory_exists:
                 main(algorithm_list=algorithm_list, data=data, graph_sizes=graph_sizes, num_graphs_per_size=num_graphs_per_size,
                      experiment=f"non-iterative algorithms with {solver}", directory=directory)
-            '''
             for mode in parallel:
-                '''
                 algorithm_list = [GCSQ(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode)]
                 directory = f"results/{data_name}/quantum/{solver}/{'parallel' if mode else 'sequential'}"
                 directory_exists = create_nested_directory(directory)
@@ -237,31 +229,39 @@ if __name__ == "__main__":
                     main(algorithm_list=algorithm_list, data=data, graph_sizes=graph_sizes,
                          num_graphs_per_size=num_graphs_per_size, experiment=f"GCS-Q with {solver} in {mode} mode",
                          directory=directory)
-                '''
                 for k in k_list:
-                    algorithm_list = [ours_iterative_exactly(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k),
-                                      #ours_iterative_at_most(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k),
-                                      k_split_GCSQ_exactly(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k),
-                                      #k_split_GCSQ_at_most(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k),
-                                      r_qubo_iterative(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k)
-                                      ]
+                    if k == 2:
+                        algorithm_list = [
+                            ours_iterative_exactly(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver,
+                                                   parallel=mode, k=k),
+                            ours_iterative_at_most(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver,
+                                                   parallel=mode, k=k),
+                            r_qubo_iterative(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver,
+                                             parallel=mode, k=k)
+                        ]
+                    elif solver == "dwave":
+                        algorithm_list = [ours_iterative_exactly(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k),
+                                          ours_iterative_at_most(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k),
+                                          k_split_GCSQ_exactly(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k),
+                                          r_qubo_iterative(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode, k=k)
+                                          ]
+                    else:
+                        algorithm_list = [
+                            ours_iterative_exactly(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver,
+                                                   parallel=mode, k=k),
+                            ours_iterative_at_most(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver,
+                                                   parallel=mode, k=k),
+                            k_split_GCSQ_exactly(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver,
+                                                 parallel=mode, k=k),
+                            k_split_GCSQ_at_most(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver,
+                                                 parallel=mode, k=k),
+                            r_qubo_iterative(seed=seed, num_graph_sizes=num_graph_sizes, solver=solver, parallel=mode,
+                                             k=k)
+                            ]
                     directory = f"results/{data_name}/quantum/{solver}/{'parallel' if mode else 'sequential'}/k={k}"
                     directory_exists = create_nested_directory(directory)
                     if directory_exists:
                         main(algorithm_list=algorithm_list, data=data, graph_sizes=graph_sizes,
                              num_graphs_per_size=num_graphs_per_size, experiment=f"k-split algorithms with {solver} in {mode} mode for k={k}",
                              directory=directory)
-
-
-        # Classical algorithms
-        '''
-        algorithm_list = []  # TODO
-        directory = f"results/{data_name}/classical"
-        directory_exists = create_nested_directory(directory)
-        if directory_exists:
-            main(algorithm_list=algorithm_list, data=data, graph_sizes=graph_sizes,
-                 num_graphs_per_size=num_graphs_per_size,
-                 experiment=f"classical algorithms",
-                 directory=directory)
-        '''
 
